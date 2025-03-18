2698acdafeb817337e4145de857259833d8dc609
frayuda <frayudaadriansyah@gmail.com>
2025-03-18 19:47:45 +0700

    be: update server js to modulars

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
 const path = require("path");
+const movieRoutes = require("./routes/movies");
 
 const app = express();
 const PORT = 3000;
-const API_KEY = process.env.API_KEY;
-const URL = process.env.URL;
 
+// middleware / fucntion yang dieksekusi sebelom request nyampe ke  endpoint
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("✅ Server is running!");
+});
+
+app.use("/movies", movieRoutes);
+app.use("/genres", movieRoutes);
 
 // Serve Frontend (Build Vite)
 // app.use(express.static(path.join(__dirname, "dist")));
@@ -19,58 +25,20 @@ app.use(express.json());
 //   res.sendFile(path.join(__dirname, "dist", "index.html"));
 // });
 
-// Hero
-app.get("/movies", async (req, res) => {
-  try {
-    const response = await axios.get(`${URL}movie/upcoming?api_key=${API_KEY}`);
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "error fetching data" });
-  }
-});
-
-// Genres
-app.get("/genresMovies", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `${URL}genre/movie/list?api_key=${API_KEY}`
-    );
-    // data here
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "error fetching data" });
-  }
-});
-
-// Release Dates
-app.get("/:id/releaseDates", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const response = await axios.get(
-      `${URL}movie/${id}/release_dates?api_key=${API_KEY}`
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "error fetching data" });
-  }
-});
-
 // Category Movie Lists : Now Playing, Popular, Top Rated, Upcoming
-https: app.get("/movies/:category", async (req, res) => {
-  const { category } = req.params;
+// app.get("/movies/:category", async (req, res) => {
+//   const { category } = req.params;
 
-  try {
-    const response = await axios.get(
-      `${URL}movie/${category}?api_key=${API_KEY}`
-    );
+//   try {
+//     const response = await axios.get(
+//       `${URL}movie/${category}?api_key=${API_KEY}`
+//     );
 
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "error fetching data" });
-  }
-});
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ message: "error fetching data" });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
